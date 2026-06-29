d7829dd97fd457f07789efe79fba68fb3bbd6590
Nisarg-jain <jainnisag98@gmail.com>
2026-06-29 21:12:25 +0530

    feat: initial commit with appwrite setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+ console.log(import.meta.env.VITE_APPWRITE_URL);
 
   return (
     <>
-      <h1>Welcome to My Blog</h1>
+      <h1>hello</h1>
     </>
   )
 }
