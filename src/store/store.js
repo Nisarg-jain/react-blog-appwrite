0a43778031abf757cfd8431ffaa675357cdf9a59
Nisarg-jain <jainnisag98@gmail.com>
2026-07-09 23:42:49 +0530

    updated store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './authSlice'; // 💡 Make sure the file path matches your project structure!
+
 const store = configureStore({
   reducer: {
-    
+    auth: authSlice, // This gives your entire app access to the authentication state
+    // post: postSlice, <-- You can add more reducers here later as your app grows!
   },
 });
+
 export default store;
