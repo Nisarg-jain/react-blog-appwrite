dd8bb1db948eeba5b0131d2752e675f0e692e0dc
Nisarg-jain <jainnisag98@gmail.com>
2026-07-21 18:08:41 +0530

    Added Logo file and logout button and also Updated Footer File

---
+++ src/components/Header/logoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Dispatch} from 'react-redux'
+import {logout} from '../../store/slices/authSlice'
+
+
+function logoutBtn() {
+    const dispatch = Dispatch();
+    const handleLogout = () => {
+       authSearvice.logout().then(() => {
+        dispatch(logout());
+       });
+    }
+
+  return (
+    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handleLogout}>
+      Logout
+    </button>
+  )
+}
+
+export default logoutBtn
---
+++ src/components/logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function logo({width = "100px"}) {
+  return (
+    <div style={{ width }}>logo</div>
+  )
+}
+
+export default logo
