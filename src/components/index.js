a7ccf437e5a4667f3ce1be9e43381c949667d603
Nisarg-jain <jainnisag98@gmail.com>
2026-07-29 19:05:43 +0530

    Created postForm.jsx file

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,7 @@ import sidebar from './sidebar';
 import logo from './logo';
 import container from './container';
 import logoutbtn from './logoutbtn';
+import Button from './button';
+import RTE from './RTE';
 
-export { header, footer, sidebar, logo, container, logoutBtn }; 
+export { header, footer, sidebar, logo, container, logoutBtn, Button, RTE }; 
