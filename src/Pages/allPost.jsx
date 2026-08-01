f7b65087f8c4c4bd674c06fb188b1c667d206901
Nisarg-jain <jainnisag98@gmail.com>
2026-08-01 18:44:01 +0530

    created Login signup Pages

---
+++ src/Pages/allPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function allPost() {
+  return (
+    <div>allPost</div>
+  )
+}
+
+export default allPost
---
+++ src/Pages/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { login as LoginComponent } from '../Components/login'
+
+function login() {
+  return (
+    <div>
+      <LoginComponent />
+    </div>
+  )
+}
+
+export default login
